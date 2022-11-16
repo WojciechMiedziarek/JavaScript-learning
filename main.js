12e123f3c44ca279b95cceeaee1f0564049aa385
Wojtek Miedziarek <wojciech.miedziarek@gmail.com>
2022-11-16 15:38:03 +0100

    JS learning

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("hello");
+
+const firstName = "Wojtek";
+const age = 32;
+const cofee = "kawa";
+
+console.log(
+  `Cześć mam na imię ${firstName} a mój wiek to ${age} lata. Lubię pić ${cofee}. Uczę się programować, żeby mimo mojego wieku, który wynosi ${age} lata, móc zmienić zawód i powiedzieć sobie ${firstName} udało Ci się.`
+);
+
+function calculate(myNumber) {
+    console.log(`dostałam ${myNumber}`);
+return myNumber*7;
+}
+const myResult = calculate(1);
+
